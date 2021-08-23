9e7f48f077513a0e4a78ec1eb1d0235dfa88707c
kings <963987632@qq.com>
2021-08-23 11:50:11 +0800

    2.0.1 颜色优化

---
+++ src/main/java/io/github/kings1990/plugin/fastrequest/util/MyColor.java
@@ -0,0 +1,15 @@
+package io.github.kings1990.plugin.fastrequest.util;
+
+import com.intellij.ui.JBColor;
+
+import java.awt.*;
+
+public class MyColor {
+    public static final JBColor green = new JBColor(new Color(89, 168, 105), new Color(73, 156, 84));
+    public static final JBColor red = new JBColor(new Color(219, 88, 96), new Color(199, 84, 80));
+    //new JBColor(new Color(56, 159, 214), new Color(240, 167, 50));
+    public static final JBColor yellow = new JBColor(new Color(237, 162, 0), new Color(202, 111, 30));
+    //idea new JBColor(new Color(56, 159, 214), new Color(53, 146, 196));
+    public static final JBColor blue = new JBColor(new Color(56, 159, 214), new Color(70, 130, 180));
+
+}
