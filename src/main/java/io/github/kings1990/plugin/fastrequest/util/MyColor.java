92b600a4bb8c30385a5e0f919ab2b0a6bdc5aaec
kings <963987632@qq.com>
2021-08-23 15:32:51 +0800

    2.0.1 color

--- src/main/java/io/github/kings1990/plugin/fastrequest/util/MyColor.java
+++ src/main/java/io/github/kings1990/plugin/fastrequest/util/MyColor.java
@@ -12,7 +12,7 @@ import java.awt.*;
  * @date 2021/08/23
  */
 public class MyColor {
-    public static final JBColor green = new JBColor(new Color(89, 168, 105), new Color(73, 156, 84));
+    public static final JBColor green = new JBColor(new Color(89, 168, 105), new Color(60, 179, 113));
     public static final JBColor red = new JBColor(new Color(219, 88, 96), new Color(199, 84, 80));
     //new JBColor(new Color(56, 159, 214), new Color(240, 167, 50));
     public static final JBColor yellow = new JBColor(new Color(237, 162, 0), new Color(202, 111, 30));
