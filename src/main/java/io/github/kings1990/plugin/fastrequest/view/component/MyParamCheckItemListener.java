7f4c9541bc0b9356acf14fc1d7a2730f8ade9131
kings <963987632@qq.com>
2021-09-11 09:03:55 +0800

    2.0.3:全选反选按钮

---
+++ src/main/java/io/github/kings1990/plugin/fastrequest/view/component/MyParamCheckItemListener.java
@@ -0,0 +1,21 @@
+package io.github.kings1990.plugin.fastrequest.view.component;
+
+import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class MyParamCheckItemListener implements ItemListener {
+    private JTable table;
+
+    public MyParamCheckItemListener(JTable table){
+        this.table = table;
+    }
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        boolean checked = e.getStateChange() == ItemEvent.SELECTED;
+        System.out.println(checked);
+        for (int x = 0, y = table.getRowCount(); x < y; x++) {
+            table.setValueAt(checked, x, 0);
+        }
+    }
+}
