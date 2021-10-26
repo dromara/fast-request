415e77ee43e232ead5e6b5be666357a32af94e4a
kings <963987632@qq.com>
2021-10-26 15:55:01 +0800

    2.0.6:whats new

---
+++ src/main/java/io/github/kings1990/plugin/fastrequest/update/WhatsNewActivity.java
@@ -0,0 +1,33 @@
+package io.github.kings1990.plugin.fastrequest.update;
+
+import com.intellij.ide.plugins.PluginManagerCore;
+import com.intellij.ide.util.RunOnceUtil;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.extensions.PluginId;
+import com.intellij.openapi.fileEditor.impl.HTMLEditorProvider;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
+import io.github.kings1990.plugin.fastrequest.util.MyResourceBundleUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class WhatsNewActivity implements StartupActivity {
+
+    @Override
+    public void runActivity(@NotNull Project project) {
+        String GITHUB_DOC_URL = "https://kings1990.github.io/restful-fast-request-doc/guide/whatsnew/";
+        String GITEE_DOC_URL = "https://kings.gitee.io/restful-fast-request-doc/guide/whatsnew/";
+        String lastVersion = PluginManagerCore.getPlugin(PluginId.getId("io.github.kings1990.FastRequest")).getVersion();
+
+        RunOnceUtil.runOnceForApp(lastVersion, () -> {
+            ApplicationManager.getApplication().invokeLater(() -> {
+                if ("zh".equals(MyResourceBundleUtil.getKey("language"))) {
+                    HTMLEditorProvider.openEditor(project, "最新变化", GITEE_DOC_URL, "<div style=\"padding-top: 1rem; margin-bottom: 0.8rem;\">Failed to load!</div> <br/><div><a href=" + GITEE_DOC_URL + " target=\"_blank\" style=\"font-size: 2rem\">Open in browser</a></div>");
+                } else {
+                    HTMLEditorProvider.openEditor(project, "What's New", GITHUB_DOC_URL, "<div style=\"padding-top: 1rem; margin-bottom: 0.8rem;\">Failed to load!</div> <br/><div><a href=" + GITHUB_DOC_URL + " target=\"_blank\" style=\"font-size: 2rem\">Open in browser</a></div>");
+                }
+            });
+        });
+
+
+    }
+}
