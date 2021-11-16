8e3ceee67adc9ec69ea94a71cb8d8b96b9b23cea
kings <963987632@qq.com>
2021-11-16 13:33:50 +0800

    2.0.7 icons

--- src/main/java/io/github/kings1990/plugin/fastrequest/action/OpenConfigAction.java
+++ src/main/java/io/github/kings1990/plugin/fastrequest/action/OpenConfigAction.java
@@ -1,18 +1,18 @@
 package io.github.kings1990.plugin.fastrequest.action;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
-import icons.PluginIcons;
 import io.github.kings1990.plugin.fastrequest.util.MyResourceBundleUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class OpenConfigAction extends AnAction {
 
     public OpenConfigAction() {
-        super(MyResourceBundleUtil.getKey("ManageConfig"), MyResourceBundleUtil.getKey("ManageConfig"), PluginIcons.ICON_CONFIG);
+        super(MyResourceBundleUtil.getKey("ManageConfig"), MyResourceBundleUtil.getKey("ManageConfig"), AllIcons.General.Settings);
     }
 
     @Override
