a0bd18bb31b49693876949358709b3a1c59ba0a9
kings <963987632@qq.com>
2021-12-06 15:43:33 +0800

    update copyright

--- src/main/java/io/github/kings1990/plugin/fastrequest/view/FastRequestToolWindow.java
+++ src/main/java/io/github/kings1990/plugin/fastrequest/view/FastRequestToolWindow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 kings1990
+ * Copyright 2021 kings1990(darkings1990@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
