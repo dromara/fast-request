5e5d3d7c0b22eea0923b2eb6f156cfc0939a205c
kings <963987632@qq.com>
2022-05-17 15:14:56 +0800

    lc

--- src/main/java/io/github/kings1990/plugin/fastrequest/view/component/CheckBoxHeader.java
+++ src/main/java/io/github/kings1990/plugin/fastrequest/view/component/CheckBoxHeader.java
@@ -1,8 +1,8 @@
 /*
  * End-User License Agreement (EULA) of Restful Fast Request
- * This End-User License Agreement ("EULA") is a legal agreement between you and KingSoft. Our EULA was created by EULA Template for Restful Fast Request.
+ * This End-User License Agreement ("EULA") is a legal agreement between you and Kings(darkings1990@gmail.com). Our EULA was created by EULA Template for Restful Fast Request.
  *
- * This EULA agreement governs your acquisition and use of our Restful Fast Request software ("Software") directly from KingSoft or indirectly through a KingSoft authorized reseller or distributor (a "Reseller").
+ * This EULA agreement governs your acquisition and use of our Restful Fast Request software ("Software") directly from Kings(darkings1990@gmail.com) or indirectly through a Kings(darkings1990@gmail.com) authorized reseller or distributor (a "Reseller").
  *
  * Please read this EULA agreement carefully before completing the installation process and using the Restful Fast Request software. It provides a license to use the Restful Fast Request software and contains warranty information and liability disclaimers.
  *
@@ -10,10 +10,10 @@
  *
  * If you are entering into this EULA agreement on behalf of a company or other legal entity, you represent that you have the authority to bind such entity and its affiliates to these terms and conditions. If you do not have such authority or if you do not agree with the terms and conditions of this EULA agreement, do not install or use the Software, and you must not accept this EULA agreement.
  *
- * This EULA agreement shall apply only to the Software supplied by KingSoft herewith regardless of whether other software is referred to or described herein. The terms also apply to any KingSoft updates, supplements, Internet-based services, and support services for the Software, unless other terms accompany those items on delivery. If so, those terms apply.
+ * This EULA agreement shall apply only to the Software supplied by Kings(darkings1990@gmail.com) herewith regardless of whether other software is referred to or described herein. The terms also apply to any Kings(darkings1990@gmail.com) updates, supplements, Internet-based services, and support services for the Software, unless other terms accompany those items on delivery. If so, those terms apply.
  *
  * License Grant
- * KingSoft hereby grants you a personal, non-transferable, non-exclusive licence to use the Restful Fast Request software on your devices in accordance with the terms of this EULA agreement.
+ * Kings(darkings1990@gmail.com) hereby grants you a personal, non-transferable, non-exclusive licence to use the Restful Fast Request software on your devices in accordance with the terms of this EULA agreement.
  *
  * You are permitted to load the Restful Fast Request software (for example a PC, laptop, mobile or tablet) under your control. You are responsible for ensuring your device meets the minimum requirements of the Restful Fast Request software.
  *
@@ -23,14 +23,14 @@
  * Reproduce, copy, distribute, resell or otherwise use the Software for any commercial purpose
  * Allow any third party to use the Software on behalf of or for the benefit of any third party
  * Use the Software in any way which breaches any applicable local, national or international law
- * use the Software for any purpose that KingSoft considers is a breach of this EULA agreement
+ * use the Software for any purpose that Kings(darkings1990@gmail.com) considers is a breach of this EULA agreement
  * Intellectual Property and Ownership
- * KingSoft shall at all times retain ownership of the Software as originally downloaded by you and all subsequent downloads of the Software by you. The Software (and the copyright, and other intellectual property rights of whatever nature in the Software, including any modifications made thereto) are and shall remain the property of KingSoft.
+ * Kings(darkings1990@gmail.com) shall at all times retain ownership of the Software as originally downloaded by you and all subsequent downloads of the Software by you. The Software (and the copyright, and other intellectual property rights of whatever nature in the Software, including any modifications made thereto) are and shall remain the property of Kings(darkings1990@gmail.com).
  *
- * KingSoft reserves the right to grant licences to use the Software to third parties.
+ * Kings(darkings1990@gmail.com) reserves the right to grant licences to use the Software to third parties.
  *
  * Termination
- * This EULA agreement is effective from the date you first use the Software and shall continue until terminated. You may terminate it at any time upon written notice to KingSoft.
+ * This EULA agreement is effective from the date you first use the Software and shall continue until terminated. You may terminate it at any time upon written notice to Kings(darkings1990@gmail.com).
  *
  * It will also terminate immediately if you fail to comply with any term of this EULA agreement. Upon such termination, the licenses granted by this EULA agreement will immediately terminate and you agree to stop all access and use of the Software. The provisions that by their nature continue and survive will survive any termination of this EULA agreement.
  *
