0a2c3e1bf249a2b27a156092f7b17a9540636868
Mr.Hope <mister-hope@outlook.com>
2022-10-13 21:13:26 +0800

    docs: bump deps

---
+++ docs/.vuepress/navbar.ts
@@ -0,0 +1,79 @@
+import { navbar } from "vuepress-theme-hope";
+
+export const enNavbar = navbar([
+  {
+    text: "Get start",
+    link: "/en/guide/getstarted/start",
+    icon: "start1",
+  },
+  {
+    text: "Features",
+    link: "/en/guide/features",
+    icon: "featuresNew",
+  },
+  {
+    text: "Script",
+    link: "/en/guide/script",
+    icon: "code-box-fill",
+  },
+  {
+    text: "History changes",
+    link: "/en/guide/history",
+    icon: "changelog",
+  },
+  {
+    text: "Buy plugin",
+    link: "/en/guide/buy",
+    icon: "buy",
+  },
+  {
+    text: "Contacts",
+    link: "/en/guide/concatGroup",
+    icon: "contactGroup",
+  },
+]);
+
+export const zhNavbar = navbar([
+  {
+    text: "快速开始",
+    link: "/guide/getstarted/start",
+    icon: "start1",
+  },
+  {
+    text: "功能",
+    link: "/guide/features",
+    icon: "featuresNew",
+  },
+  {
+    text: "脚本",
+    link: "/guide/script",
+    icon: "code-box-fill",
+  },
+  {
+    text: "历史变更",
+    link: "/guide/history",
+    icon: "changelog",
+  },
+  {
+    text: "支持插件",
+    link: "/guide/buy",
+    icon: "buy",
+  },
+  {
+    text: "视频教程",
+    link: "/guide/teachingVideo",
+    icon: "bilibili",
+  },
+  {
+    text: "🔥 加入群聊",
+    link: "/guide/concatGroup",
+  },
+  {
+    text: "❤️ 赞助",
+    link: "/guide/sponsorList",
+  },
+  {
+    text: "🎁 活动",
+    link: "/guide/activity",
+  },
+]);
