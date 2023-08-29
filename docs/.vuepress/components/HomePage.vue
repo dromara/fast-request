51496c2fe47c7d0c9da2a3cc2d8f1f5595d9690e
kings <963987632@qq.com>
2023-08-29 19:22:25 +0800

    2023.1.9

--- docs/.vuepress/components/HomePage.vue
+++ docs/.vuepress/components/HomePage.vue
@@ -1,11 +1,11 @@
 <template>
-  <NormalPage>
-    <template #contentBefore>
+  <HopeHomePage>
+    <template #center>
       <AdPage content="content" />
     </template>
-  </NormalPage>
+  </HopeHomePage>
 </template>
 <script setup lang="ts">
-import NormalPage from "vuepress-theme-hope/components/NormalPage.js";
-import AdPage from "./AdPage.vue"
+import HopeHomePage from "vuepress-theme-hope/components/HomePage.js";
+import AdPage from "./AdPage.vue";
 </script>
