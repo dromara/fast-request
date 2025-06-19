2bc487081c549803628d6b58567306d8ddf5c4b3
Kings <963987632@qq.com>
2025-06-19 17:15:59 +0800

    update build

--- src/main/java/io/github/kings1990/plugin/fastrequest/contributor/FastRequestEqualityProvider.java
+++ src/main/java/io/github/kings1990/plugin/fastrequest/contributor/FastRequestEqualityProvider.java
@@ -1,44 +1,20 @@
 /*
  *
- *  * End-User License Agreement (EULA) of Restful Fast Request
- *  * This End-User License Agreement ("EULA") is a legal agreement between you and Kings(darkings1990@gmail.com). Our EULA was created by EULA Template for Restful Fast Request.
+ *  * Copyright (C) 2021 WangSheng darkings1990@gmail.com
  *  *
- *  * This EULA agreement governs your acquisition and use of our Restful Fast Request software ("Software") directly from Kings(darkings1990@gmail.com) or indirectly through a Kings(darkings1990@gmail.com) authorized reseller or distributor (a "Reseller").
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU Affero General Public License as published
+ *  * by the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
  *  *
- *  * Please read this EULA agreement carefully before completing the installation process and using the Restful Fast Request software. It provides a license to use the Restful Fast Request software and contains warranty information and liability disclaimers.
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU Affero General Public License for more details.
  *  *
- *  * If you register for a free trial of the Restful Fast Request software, this EULA agreement will also govern that trial. By clicking "accept" or installing and/or using the Restful Fast Request software, you are confirming your acceptance of the Software and agreeing to become bound by the terms of this EULA agreement.
- *  *
- *  * If you are entering into this EULA agreement on behalf of a company or other legal entity, you represent that you have the authority to bind such entity and its affiliates to these terms and conditions. If you do not have such authority or if you do not agree with the terms and conditions of this EULA agreement, do not install or use the Software, and you must not accept this EULA agreement.
- *  *
- *  * This EULA agreement shall apply only to the Software supplied by Kings(darkings1990@gmail.com) herewith regardless of whether other software is referred to or described herein. The terms also apply to any Kings(darkings1990@gmail.com) updates, supplements, Internet-based services, and support services for the Software, unless other terms accompany those items on delivery. If so, those terms apply.
- *  *
- *  * License Grant
- *  * Kings(darkings1990@gmail.com) hereby grants you a personal, non-transferable, non-exclusive licence to use the Restful Fast Request software on your devices in accordance with the terms of this EULA agreement.
- *  *
- *  * You are permitted to load the Restful Fast Request software (for example a PC, laptop, mobile or tablet) under your control. You are responsible for ensuring your device meets the minimum requirements of the Restful Fast Request software.
- *  *
- *  * You are not permitted to:
- *  *
- *  * Edit, alter, modify, adapt, translate or otherwise change the whole or any part of the Software nor permit the whole or any part of the Software to be combined with or become incorporated in any other software, nor decompile, disassemble or reverse engineer the Software or attempt to do any such things
- *  * Copy this project and republish a new plugin in JetBrains Marketplace
- *  * Reproduce, copy, distribute, resell or otherwise use the Software for any commercial purpose
- *  * Allow any third party to use the Software on behalf of or for the benefit of any third party
- *  * Use the Software in any way which breaches any applicable local, national or international law
- *  * use the Software for any purpose that Kings(darkings1990@gmail.com) considers is a breach of this EULA agreement
- *  * Intellectual Property and Ownership
- *  * Kings(darkings1990@gmail.com) shall at all times retain ownership of the Software as originally downloaded by you and all subsequent downloads of the Software by you. The Software (and the copyright, and other intellectual property rights of whatever nature in the Software, including any modifications made thereto) are and shall remain the property of Kings(darkings1990@gmail.com).
- *  *
- *  * Kings(darkings1990@gmail.com) reserves the right to grant licences to use the Software to third parties.
- *  *
- *  * Termination
- *  * This EULA agreement is effective from the date you first use the Software and shall continue until terminated. You may terminate it at any time upon written notice to Kings(darkings1990@gmail.com).
- *  *
- *  * It will also terminate immediately if you fail to comply with any term of this EULA agreement. Upon such termination, the licenses granted by this EULA agreement will immediately terminate and you agree to stop all access and use of the Software. The provisions that by their nature continue and survive will survive any termination of this EULA agreement.
- *  *
- *  * Governing Law
- *  * This EULA agreement, and any dispute arising out of or in connection with this EULA agreement, shall be governed by and construed in accordance with the laws of cn.
- *
+ *  * You should have received a copy of the GNU Affero General Public License
+ *  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
  */
 
 package io.github.kings1990.plugin.fastrequest.contributor;
